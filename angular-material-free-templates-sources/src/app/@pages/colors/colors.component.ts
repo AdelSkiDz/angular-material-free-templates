890e25983fd75967a467968a5cb893cbed3b65de
Adel BOUMAZA [53288a54-9a95-470b-a96b-da1d0be86492] <Adel.BOUMAZA@ca-ps.com>
2022-08-26 15:29:41 +0200

    feat: pages colors

--- angular-material-free-templates-sources/src/app/@pages/colors/colors.component.ts
+++ angular-material-free-templates-sources/src/app/@pages/colors/colors.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {AppService} from "../../@core/services/app.service";
 import {Route} from "../page-routing.module";
 import {ContentTopValueComponent} from "../layout/content-top/content-top-value.component";
+import {PeriodicElement} from "../typography/typography.component";
 
 @Component({
   selector: 'app-colors',
@@ -9,7 +10,18 @@ import {ContentTopValueComponent} from "../layout/content-top/content-top-value.
   styleUrls: ['./colors.component.scss']
 })
 export class ColorsComponent extends ContentTopValueComponent {
-
+  dataSource = ELEMENT_DATA;
+  dataSourceHtml = ELEMENT_DATA_HTML;
+  codeSCSS = `
+   $theme-colors: (
+      "warning":    $app-warning,
+      "success":    $app-success,
+      "danger":     $app-danger,
+      "optional":   $app-optional,
+      "default":    $app-default,
+      "info":       $app-info,
+   );
+ `
   constructor(_appService: AppService) {
     super(_appService);
   }
@@ -23,5 +35,31 @@ export class ColorsComponent extends ContentTopValueComponent {
       {actived: true, name: Route.Colors, routerLink: Route.Colors}
     ];
   }
-
 }
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {header: 'app-warning-bg', value: 'linear-gradient(60deg,#ffa726,#fb8c00)'},
+  {header: 'app-success-bg', value: 'linear-gradient(60deg,#66bb6a,#43a047)'},
+  {header: 'app-danger-bg', value: 'linear-gradient(60deg,#ef5350,#e53935)'},
+  {header: 'app-optional-bg', value: 'linear-gradient(60deg,#ab47bc,#8e24aa)'},
+  {header: 'app-default-bg', value: '#999'},
+  {header: 'app-info-bg', value: '#00bcd4'}
+];
+
+const ELEMENT_DATA_HTML: PeriodicElement[] = [
+  {header: '<div class="app-warning-bg"></div>'},
+  {header: '<div class="app-success-bg"></div>'},
+  {header: '<div class="app-danger-bg"></div>'},
+  {header: '<div class="app-optional-bg"></div>'},
+  {header: '<div class="app-default-bg"></div>'},
+  {header: '<div class="app-info-bg"></div>'},
+  {header: '<div class="app-primary-bg"></div>'}
+];
+
+
+
+
+
+
+
+
